f633fcdf4dd30376c5bf762b28fd4a176b211d04
Colder <Billy@cometnet-10-21-40-180.utdallas.edu>
2017-04-01 20:37:52 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :related_dates
   resources :loans
-  resources :borrowers
+  root 'loans#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
